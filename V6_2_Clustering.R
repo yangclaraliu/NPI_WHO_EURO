5842550de17d4e94e0334764f6fb8399f45b767d
Charlie Diamond <charlie.diamond@lshtm.ac.uk>
2021-11-19 18:55:29 +0000

    V6 multi-level and max effort, cluster and effect

---
+++ V6_2_Clustering.R
@@ -0,0 +1,370 @@
+#if(!exists("joined")) joined <- here("data", "joined_all.RDS") %>% readRDS
+
+joined <- readRDS("data/joined_all_V6.RDS")
+
+# Remove vaccination from wild type scenarios 
+joined$s1_W_mid <- joined$s1_W_mid[,1:17]
+joined$s1_W_max <- joined$s1_W_max[,1:17]
+
+
+# Full number of countries, no age stratification
+policy_raw_all <- c("C1","C2","C3","C4","C5","C6","C7","C8","E1","E2","H1","H2","H3","H6","V_all_adj")
+
+var_remove <- c("country", "cnt", "date","V_all_adj", "V_18_60_adj", "V_60_adj", "V_tot_adj", "median", "lower_90", "upper_90", "lower_50", "upper_50")
+
+#policy_raw_age <- c("C1","C2","C3","C4","C5","C6","C7","C8","E1","E2","H1","H2","H3","H6","V_18_60_adj", "V_60_adj")
+
+
+# [START]
+# this needs to be ran only once, and it may take some time
+# this segment conducts the temporal clustering analyses.
+
+# Full TS, Alpha and Delta variants
+hcd_FAD <-  joined %>%
+  .[c("s1_full_mid", "s1_A_mid","s1_D_mid",
+      "s1_full_max", "s1_A_max","s1_D_max")] %>%
+  map(ungroup) %>%
+  map(dplyr::select, policy_raw_all) %>%
+  map(pvclust::pvclust,
+      method.hclust = "ward.D2",
+      method.dist = "euclidean",
+      nboot = 10000)
+# 
+save(hcd_FAD, file = "results/hcd_V6_FAD.rdata")
+
+# Wild type
+hcd_W <-  joined %>%
+  .[c("s1_W_mid","s1_W_max")] %>%
+  map(ungroup) %>%
+  map(dplyr::select, -var_remove) %>%
+  map(pvclust::pvclust,
+      method.hclust = "ward.D2",
+      method.dist = "euclidean",
+      nboot = 10000)
+# 
+save(hcd_W, file = "results/hcd_V6_W.rdata")
+
+# [END]
+
+joined %>%
+  .[c("s1_A_mid")] %>%
+  map(ungroup) %>%
+  map(dplyr::select, policy_raw_all) %>%
+  map(pvclust::pvclust,
+      method.hclust = "ward.D2",
+      method.dist = "euclidean",
+      nboot = 10000)
+
+joined %>%
+  .[c("s1_A_mid")] %>% 
+  ungroup()
+
+  map(dist, method = "euclidean") %>% 
+  map(hclust, method = "ward.D2")
+
+joined$s1_A_mid %>% 
+  ungroup() %>% 
+  select(policy_raw_all) %>% 
+  t() %>% 
+  pvclust( method.hclust = "ward.D2",
+           method.dist = "euclidean",
+           nboot = 100)
+
+
+# Multi-level effort
+# Full TS
+plot(hcd_FAD$s1_full_mid)
+pvrect(hcd_FAD$s1_full_mid, alpha=0.95)
+
+# Wild type
+plot(hcd_W$s1_W_mid)
+pvrect(hcd_W$s1_W_mid, alpha=0.95)
+
+# Alpha 
+plot(hcd_FAD$s1_A_mid)
+pvrect(hcd_FAD$s1_A_mid, alpha=0.95)
+
+# Delta
+plot(hcd_FAD$s1_D_mid)
+pvrect(hcd_FAD$s1_D_mid, alpha=0.95)
+
+
+# Max effort
+# Full Ts
+plot(hcd_FAD$s1_full_max)
+pvrect(hcd_FAD$s1_full_max, alpha=0.95)
+
+# Wild type
+plot(hcd_W$s1_W_max)
+pvrect(hcd_W$s1_W_max, alpha=0.95)
+
+# Alpha 
+plot(hcd_FAD$s1_A_max)
+pvrect(hcd_FAD$s1_A_max, alpha=0.95)
+
+# Delta 
+plot(hcd_FAD$s1_D_max)
+pvrect(hcd_FAD$s1_D_max, alpha=0.95)
+
+
+# full alpha and delta
+list_FAD <- joined %>%
+  .[c("s1_full_mid", "s1_A_mid","s1_D_mid",
+      "s1_full_max", "s1_A_max","s1_D_max")] %>%
+  map(ungroup) %>%
+  map(dplyr::select, policy_raw_all) %>% 
+  map(t) %>% 
+  map(dist, method = "euclidean") %>% 
+  map(hclust, method = "ward.D2") %>% 
+  map(dendro_data)
+
+# wild
+list_w <- joined %>%
+  .[c("s1_W_mid","s1_W_max")] %>%
+  map(ungroup) %>%
+  map(dplyr::select, policy_raw_all) %>% 
+  map(t) %>% 
+  map(dist, method = "euclidean") %>% 
+  map(hclust, method = "ward.D2") %>% 
+  map(dendro_data)
+
+
+# List of all age clustering 
+all_age_dend <- list_FAD
+
+
+
+# Full TS multi-level effort
+plot(hcd_FAD$s1_full_mid)
+pvrect(hcd_FAD$s1_full_mid, alpha=0.95)
+
+plot_dend_all_age(1)+
+  labs(title = "Multi-level effort - Full time series\n2020-01-01 to 2021-09-30",
+       color = " ",
+       y = "Height") +
+  geom_rect(data = data.frame(xmin = c(4.65, 0.25),
+                              xmax = c(15.75, 4.45),
+                              ymin = c(-65, -65),
+                              ymax = c(105, 95)),
+            aes(xmin = xmin,
+                xmax = xmax,
+                ymin = ymin,
+                ymax = ymax),
+            fill = NA,
+            color = "firebrick",
+            size = 1.5,
+            linetype = 2) +
+  geom_segment(aes(x = 0, xend = 0, y = 0, yend = 150), size = 1.5) +
+  geom_text(aes(y = c(0, 50, 75,100, 150), 
+                x = -0.2, 
+                label = c("0","50","\nHeight","100","150")), 
+            size = 5)
+
+ggsave("figs/EURO_V6/fig2_1_mid_full.png",
+       width = 18,
+       height = 9)  
+
+
+# Alpha, mult-level effort
+plot(hcd_FAD$s1_A_mid)
+pvrect(hcd_FAD$s1_A_mid, alpha=0.95)
+
+plot_dend_all_age(1)+
+  labs(title = "Alpha dominant period\n2020-01-01 to 2021-09-30",
+       color = " ",
+       y = "Height") +
+  geom_rect(data = data.frame(xmin = c(4.65, 0.25),
+                              xmax = c(15.75, 4.45),
+                              ymin = c(-65, -65),
+                              ymax = c(60, 60)),
+            aes(xmin = xmin,
+                xmax = xmax,
+                ymin = ymin,
+                ymax = ymax),
+            fill = NA,
+            color = "firebrick",
+            size = 1.5,
+            linetype = 2) +
+  geom_segment(aes(x = 0, xend = 0, y = 0, yend = 150), size = 1.5) +
+  geom_text(aes(y = c(0, 50, 75,100, 150), 
+                x = -0.2, 
+                label = c("0","50","\nHeight","100","150")), 
+            size = 5)
+
+ggsave("figs/EURO_V6/fig2_1_mid_full.png",
+       width = 18,
+       height = 9)  
+
+# S1 Delta variant
+hcd_all$s1_D %>% plot()
+
+plot_dend_all_age(3)+
+  labs(title = "Delta variant\n2021-05-11 to 2021-09-30",
+       color = " ",
+       y = "Height") +
+  geom_rect(data = data.frame(xmin = c(3.65, 0.25),
+                              xmax = c(15.75, 3.45),
+                              ymin = c(-65, -65),
+                              ymax = c(70, 35)),
+            aes(xmin = xmin,
+                xmax = xmax,
+                ymin = ymin,
+                ymax = ymax),
+            fill = NA,
+            color = "firebrick",
+            size = 1.5,
+            linetype = 2) +
+  geom_segment(aes(x = 0, xend = 0, y = 0, yend = 150), size = 1.5) +
+  geom_text(aes(y = c(0, 50, 75,100, 150), 
+                x = -0.2, 
+                label = c("0","50","\nHeight","100","150")), 
+            size = 5)
+
+ggsave("figs/EURO_V5/fig2_3_delta.png",
+       width = 18,
+       height = 9)  
+
+
+
+### ---- ####  Age clustering  ### ---- ####  
+
+hcd_age <-  joined %>%
+  .[c("s1_W", "s1_A", "s1_D")] %>%
+  map(ungroup) %>%
+  map(filter, !country %in% cnt_remove ) %>% 
+  map(dplyr::select, policy_raw_age) %>%
+  
+  # this specific line below is for calculating the statistical significance of 
+  # temporal clusters based on bootstrapping.
+  
+  map(pvclust::pvclust,
+      method.hclust = "ward.D2",
+      method.dist = "euclidean",
+      nboot = 10000)
+
+save(hcd_age, file = "results/hcd_age_V5.rdata")
+
+
+
+# S1 cluster - wild
+s1_W_dend_age <- joined$s1_W %>% 
+  filter(!country %in% cnt_remove) %>% 
+  select(policy_raw_age) %>% 
+  t() %>% 
+  dist(method = "euclidean") %>% 
+  hclust(method = "ward.D2") %>% 
+  dendro_data()
+
+# S1 cluster - Alpha
+s1_A_dend_age <- joined$s1_A %>% 
+  filter(!country %in% cnt_remove) %>% 
+  select(policy_raw_age) %>% 
+  t() %>% 
+  dist(method = "euclidean") %>% 
+  hclust(method = "ward.D2") %>% 
+  dendro_data()
+
+
+# S1 cluster - Delta
+s1_D_dend_age <- joined$s1_D %>% 
+  filter(!country %in% cnt_remove) %>% 
+  select(policy_raw_age) %>% 
+  t() %>% 
+  dist(method = "euclidean") %>% 
+  hclust(method = "ward.D2") %>% 
+  dendro_data()
+
+
+
+# List of all age clustering 
+age_dend <- list(s1_W_dend_age, s1_A_dend_age, s1_D_dend_age)
+
+
+# S1 wild virus age
+hcd_age$s1_W %>% plot()
+
+plot_dend_age(1)+
+  labs(title = "Wild virus\n2020-01-01 to 2020-11-22",
+       color = " ",
+       y = "Height") +
+  geom_rect(data = data.frame(xmin = c(6.65, 0.25),
+                              xmax = c(16.75, 6.45),
+                              ymin = c(-65, -65),
+                              ymax = c(65, 60)),
+            aes(xmin = xmin,
+                xmax = xmax,
+                ymin = ymin,
+                ymax = ymax),
+            fill = NA,
+            color = "firebrick",
+            size = 1.5,
+            linetype = 2) +
+  geom_segment(aes(x = 0, xend = 0, y = 0, yend = 150), size = 1.5) +
+  geom_text(aes(y = c(0, 50, 75,100, 150), 
+                x = -0.2, 
+                label = c("0","50","\nHeight","100","150")), 
+            size = 5)
+
+ggsave("figs/EURO_V5/fig2_1_wild_age.png",
+       width = 18,
+       height = 9)  
+
+
+# S1 Alphavirus age
+hcd_age$s1_A %>% plot()
+
+plot_dend_age(2)+
+  labs(title = "Alpha variant\n2020-11-23 to 2021-05-10",
+       color = " ",
+       y = "Height") +
+  geom_rect(data = data.frame(xmin = c(11.65, 0.25),
+                              xmax = c(16.75, 11.45),
+                              ymin = c(-65, -65),
+                              ymax = c(55, 50)),
+            aes(xmin = xmin,
+                xmax = xmax,
+                ymin = ymin,
+                ymax = ymax),
+            fill = NA,
+            color = "firebrick",
+            size = 1.5,
+            linetype = 2) +
+  geom_segment(aes(x = 0, xend = 0, y = 0, yend = 150), size = 1.5) +
+  geom_text(aes(y = c(0, 50, 75,100, 150), 
+                x = -0.2, 
+                label = c("0","50","\nHeight","100","150")), 
+            size = 5)
+
+ggsave("figs/EURO_V5/fig2_2_alpha_age.png",
+       width = 18,
+       height = 9)  
+
+
+# S1 Delta variant
+hcd_age$s1_D %>% plot()
+
+plot_dend_age(3)+
+  labs(title = "Delta variant\n2021-05-11 to 2021-09-30",
+       color = " ",
+       y = "Height") +
+  geom_rect(data = data.frame(xmin = c(3.65, 0.25),
+                              xmax = c(16.75, 3.45),
+                              ymin = c(-65, -65),
+                              ymax = c(50, 30)),
+            aes(xmin = xmin,
+                xmax = xmax,
+                ymin = ymin,
+                ymax = ymax),
+            fill = NA,
+            color = "firebrick",
+            size = 1.5,
+            linetype = 2) +
+  geom_segment(aes(x = 0, xend = 0, y = 0, yend = 150), size = 1.5) +
+  geom_text(aes(y = c(0, 50, 75,100, 150), 
+                x = -0.2, 
+                label = c("0","50","\nHeight","100","150")), 
+            size = 5)
+
+ggsave("figs/EURO_V5/fig2_3_delta_age.png",
+       width = 18,
+       height = 9)  
